ebb651a02f3e242c00daf41f40de9b04cad5478c
Topher <topherb99@gmail.com>
2019-10-16 17:23:59 +0100

    day 2 progress with ingrid added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,30 @@
 require_relative 'bike'
 
 class DockingStation
-
-
-  def release_bike
-    Bike.new
+  attr_reader :bike
+  def initialize
+    @bikes = []
   end
 
+  def release_bike
+    fail 'no bikes available' if empty?
+    @bikes.pop
+  end
 
+  def dock(bike)
+    @bike = bike
+    fail 'Docking station full' if full?
+    @bikes << bike
+    bike
+  end
 
+  private
 
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty? 
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,33 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+    it 'docks something' do
+     bike = Bike.new
+     expect(subject.dock(bike)).to eq bike
+    end
+    it { is_expected.to respond_to(:bike) }
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'no bikes available'
+    end
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+  describe '#dock' do
+    it 'raises an error when full' do
+      DockingStation::DEFAULT_CAPACITY.times do
+        subject.dock Bike.new
+      end
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
   end
 end
